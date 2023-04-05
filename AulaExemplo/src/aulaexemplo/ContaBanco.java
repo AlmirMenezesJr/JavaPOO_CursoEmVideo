d66d02f6f655cb21ce65626f117543f5e24107b3
AlmirMenezesJr <jrjmswii@gmail.com>
2023-04-05 00:19:04 -0300

    AULA 05 NOVA MODIFICAÇÃO

--- AulaExemplo/src/aulaexemplo/ContaBanco.java
+++ AulaExemplo/src/aulaexemplo/ContaBanco.java
@@ -1,18 +1,28 @@
 
 package aulaexemplo;
 
-public class Conta {
+public class ContaBanco {
+    //atributos
     public int numeroConta;
     private String nome;
     protected String tipo;
     private double saldo;
     private boolean status;
     
-    public Conta(String n){
-        this.numeroConta++; 
+    //construtor
+    
+    public ContaBanco(){
         this.status = false;
-        this.nome = n;
         this.saldo = 0;
+        
+    }
+        
+    public int getNumeroConta() {
+        return numeroConta;
+    }
+
+    public void setNumeroConta(int numeroConta) {
+        this.numeroConta = numeroConta;
     }
 
     public String getNome() {
@@ -39,7 +49,7 @@ public class Conta {
         this.saldo = saldo;
     }
 
-    public boolean isStatus() {
+    public boolean getStatus() {
         return status;
     }
 
@@ -48,18 +58,21 @@ public class Conta {
     }
     
     public void abrirConta(String t){
-        this.status = true;
+        this.setStatus(true);
         if (t.equals("CC")){
-            this.saldo = 50;
+            this.setTipo("CC");
+            this.setSaldo(50);
         } else if (t.equals("CP")){
-            this.saldo = 150;
+            this.setTipo("CP");
+            this.setSaldo(150);
         }
+        System.out.println("Conta Aberta!");
     }
     
     public void fecharConta(){
-        if (this.saldo > 0){
+        if (this.getSaldo() > 0){
            System.out.println("Impossível fechar conta com saldo positivo.");
-        } else if(this.saldo < 0){
+        } else if(this.getSaldo() < 0){
             System.out.println("Impossível fechar conta com saldo negativo.");
         } else{    
             this.status = false;
@@ -67,36 +80,42 @@ public class Conta {
         }
     }
     public void depositar(double v){
-        if(status){
-            this.saldo = this.saldo + v;
+        if(getStatus()){
+            this.setSaldo(this.getSaldo() + v);
             System.out.println("Deposito realizado no valor R$"+v);
         } else {
-            System.out.println("Erro. conta inativa.");
+            System.out.println("Erro. conta Fechada.");
         }
     }
     
     public void sacar(double v){
-        if(status && this.saldo>v){
-            this.saldo = this.saldo - v;
+        if(getStatus() && this.getSaldo()>=v){
+            this.setSaldo(this.getSaldo() - v);
             System.out.println("saque realizado no valor R$"+v);
-        } else if(this.saldo < v){
+        } else if(this.getSaldo() < v){
             System.out.println("Saldo Insuficiente");
         } else {
-            System.out.println("Erro. conta inativa.");
+            System.out.println("Erro. conta fechada.");
         }
     }
     
     public void pagarMensalidade(){
-        if (status && this.tipo.equals("CC")){
-            this.saldo = this.saldo - 12;
-        } else if(status && this.tipo.equals("CP")){
-            this.saldo = this.saldo - 20;
+        if (getStatus() && this.getTipo().equals("CC")){
+            this.setSaldo(this.getSaldo() - 12);
+        } else if(getStatus() && this.getTipo().equals("CP")){
+            this.setSaldo(this.getSaldo() - 20);
         }
     }
     
-     public void status(){
-         System.out.printf("Olá %s, como vai?", this.nome);
-         System.out.println("Seu saldo é R$:"+ this.getSaldo());          
+     public void estadoAtual(){
+         System.out.printf("Ola %s, como vai? %n", this.nome);
+         System.out.printf("Conta: %s%n", this.numeroConta);
+         System.out.printf("O tipo da conta e:  %s%n", this.getTipo());
+         System.out.printf("Seu saldo e R$: %s%n", this.getSaldo()); 
+         System.out.printf("Estado da conta %s%n", this.getStatus());
+         System.out.println(getTipo());
+         
      }
              
+     
 }
--- AulaExemplo/src/aulaexemplo/ExemploBanco.java
+++ AulaExemplo/src/aulaexemplo/ExemploBanco.java
@@ -5,12 +5,23 @@ public class ExemploBanco{
 
     public static void main(String[] args) {
         
-        Conta user1 = new Conta("Almir Menezes");
-        Conta user2 = new Conta("natinha");
+        ContaBanco user1 = new ContaBanco();
+        ContaBanco user2 = new ContaBanco();
+        user1.setNumeroConta(0001);
+        user1.setNome("Almir Menezes Junior");
+        user1.setNumeroConta(0002);
+        user2.setNome("Natalia Santos Menezes");
+        
         user1.abrirConta("CP");
         user2.abrirConta("CC");
-        user1.status();
-        user2.status();
+        
+        
+        
+        user1.estadoAtual();
+        user2.estadoAtual();
+        
+        user1.sacar(50);
+        user2.fecharConta();
         
         
         
